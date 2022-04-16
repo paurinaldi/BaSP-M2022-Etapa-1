393d1ddfd0efd5215020000ebeafccfc6ff930c1
Paula Rinaldi <paurinaldi95@gmail.com>
2022-04-16 12:43:51 -0300

    “BaSP-2022-W5: On ex01 I improved the name of the strings and translated all code to english, on ex03 I fixed the quotation marks so it is consistent with the rest of the exercises.

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -3,25 +3,25 @@
 // a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos
 // números en una 3er variable.
 
-var a = 4;
-var b = 5;
-var c = a + 5;
-console.log('Respuesta01_a:', c);
+var numericVariable1 = 4;
+var numericVariable2 = 5;
+var sumOfVariables = numericVariable1 + numericVariable2;
+console.log('Solution01_a:', sumOfVariables);
 
 // b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
-var d = 'Variable de ';
-var e = 'tipo string.';
-var f = d + e;
-console.log('Respuesta01_b:', f);
+var stringVariable1 = 'String type ';
+var stringVariable2 = 'variable.';
+var sumOfStrings = stringVariable1 + stringVariable2;
+console.log('Solution01_b:', sumOfStrings);
 
 
 // c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string),
 // guardando el resultado de la suma en una 3er variable (utilizar length)
 
-var g = 'Hola, como ';
-var h = 'estas?';
-var j = g.length + h.length;
+var stringLength1 = 'Hi, how ';
+var stringLenght2 = 'are you?';
+var sumOfLenght = stringLength1.length + stringLenght2.length;
 
-console.log('Respuesta01_c:', j);
+console.log('Solution01_c:', sumOfLenght);
 
--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -1,52 +1,52 @@
 //Arrays
 
-//a. Dado el siguiente array:["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto",
-//"Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11(utilizar console.log).
+//a. Dado el siguiente array:['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+//'Octubre', 'Noviembre', 'Diciembre'] mostrar por consola los meses 5 y 11(utilizar console.log).
 
-var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"];
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre']; 
 console.log('Solution03_a:', months[4], months[10]);
 
 //b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
 
-var monthsToAlphaOrder = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"]; 
+var monthsToAlphaOrder = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre']; 
 monthsToAlphaOrder.sort();
 console.log('Solution03_b:', monthsToAlphaOrder);
 
 //c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
 
-var monthsToAdd = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"]; 
+var monthsToAdd = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre']; 
 monthsToAdd.push('post Diciembre');
 monthsToAdd.unshift('pre Enero');
 console.log('Solution03_c:', monthsToAdd);
 
 //d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
 
-var monthsToEliminate = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"]; 
+var monthsToEliminate = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre']; 
 monthsToEliminate.pop();
 monthsToEliminate.shift();
 console.log('Solution04_d:', monthsToEliminate);
 
 //e. Invertir el orden del array (utilizar reverse).
 
-var monthsToReverse = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"]; 
+var monthsToReverse = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre']; 
 monthsToReverse.reverse()
 console.log('Solution04_e', monthsToReverse);
 
 //f.Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
 
-var monthsToJoin = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"];
+var monthsToJoin = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre']; 
 var monthsJoined = monthsToJoin.join('-');
 console.log('Solution04_f:', monthsJoined);
 
 //g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
 
-var monthsToSlice = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto","Septiembre",
-"Octubre", "Noviembre", "Diciembre"];
+var monthsToSlice = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
 var monthsSliced = monthsToSlice.slice(4, 11)
 console.log('Solution04_g:', monthsSliced);
