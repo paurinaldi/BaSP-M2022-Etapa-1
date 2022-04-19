3397b1c3aa9d9c2092bfa8e42aed632d4860c0df
Paula Rinaldi <paurinaldi95@gmail.com>
2022-04-19 12:03:38 -0300

    BaSP-2022-W5:ex03-arrays: Translated array's elements to english.

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -3,50 +3,50 @@
 //a. Dado el siguiente array:['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
 //'Octubre', 'Noviembre', 'Diciembre'] mostrar por consola los meses 5 y 11(utilizar console.log).
 
-var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre']; 
+var months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December']; 
 console.log('Solution03_a:', months[4], months[10]);
 
 //b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
 
-var monthsToAlphaOrder = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre']; 
+var monthsToAlphaOrder = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December']; 
 monthsToAlphaOrder.sort();
 console.log('Solution03_b:', monthsToAlphaOrder);
 
 //c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
 
-var monthsToAdd = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre']; 
-monthsToAdd.push('post Diciembre');
-monthsToAdd.unshift('pre Enero');
+var monthsToAdd = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December']; 
+monthsToAdd.push('post December');
+monthsToAdd.unshift('pre January');
 console.log('Solution03_c:', monthsToAdd);
 
 //d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
 
-var monthsToEliminate = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre']; 
+var monthsToEliminate = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December']; 
 monthsToEliminate.pop();
 monthsToEliminate.shift();
 console.log('Solution04_d:', monthsToEliminate);
 
 //e. Invertir el orden del array (utilizar reverse).
 
-var monthsToReverse = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre']; 
+var monthsToReverse = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December']; 
 monthsToReverse.reverse()
-console.log('Solution04_e', monthsToReverse);
+console.log('Solution04_e:', monthsToReverse);
 
 //f.Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
 
-var monthsToJoin = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre']; 
+var monthsToJoin = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December']; 
 var monthsJoined = monthsToJoin.join('-');
 console.log('Solution04_f:', monthsJoined);
 
 //g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
 
-var monthsToSlice = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre',
-'Octubre', 'Noviembre', 'Diciembre'];
+var monthsToSlice = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August','September',
+'October', 'November', 'December'];
 var monthsSliced = monthsToSlice.slice(4, 11)
 console.log('Solution04_g:', monthsSliced);
